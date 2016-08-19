7e7e8353d1c6dc04f1e0756acd6bf610b8186830
Moe Aboulkheir <moe.aboulkheir@gmail.com>
2016-08-19 12:55:59 +0100

    0.1.0

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject io.nervous/cljs-rollbar "0.1.0-SNAPSHOT"
+(defproject io.nervous/cljs-rollbar "0.1.0"
   :description "Clojurescript/Node friendly Rollbar client"
   :url "https://github.com/nervous-systems/cljs-rollbar"
   :license {:name "Unlicense" :url "http://unlicense.org/UNLICENSE"}
   :dependencies [[org.clojure/clojure       "1.8.0"]
                  [org.clojure/clojurescript "1.8.34"]
-                 [io.nervous/kvlt           "0.1.3-SNAPSHOT"]]
+                 [io.nervous/kvlt           "0.1.3"]]
   :plugins [[lein-npm "0.6.0"]])
